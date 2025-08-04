2ed753f3b504db22a7b3778be118713daf7aec5c
JarilynTenGrand <tenerefe.jarilyn1010@gmail.com>
2025-08-04 18:16:40 +0800

    Add Basic Cards in the Dashboard

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import Card from "../../components/Cards";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>Home</div>
+      <div className="card-container">
+        <Card
+          imgSrc="https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg"
+          data="10XX"
+          title="Product Transaction"
+        />
+        <Card
+          imgSrc="https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg"
+          data="20XX"
+          title="Sales"
+        />
+        <Card
+          imgSrc="https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg"
+          data="30XX"
+          title="Number of Stock In"
+        />
+        <Card
+          imgSrc="https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg"
+          data="40XX"
+          title="Number of Spoiled and Damaged Ingredients"
+        />
+      </div>
 
-export default Home
+      {/* For Sale and Best Selling Card */}
+      <div className="card-container">
+        <Card
+          imgSrc="https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg"
+          data="40XX"
+          title="Sales"
+        />
+        <Card
+          imgSrc="https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg"
+          data="40XX"
+          title="Best Selling Products"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Home;
